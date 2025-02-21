836e8b65fec30e0dc0b911824c9ce03f90ac4cb0
muhamademiralfani <muhamademiralfani@gmail.com>
2025-02-22 00:30:03 +0700

    feat(components) : add Sidebar component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Table } from "./commons/Table";
 export { default as TableCell } from "./commons/TableCell";
 export { default as Card } from "./commons/Card";
 export { default as EducationForm } from "./education/EducationForm";
+export { default as Sidebar} from './commons/Sidebar'
